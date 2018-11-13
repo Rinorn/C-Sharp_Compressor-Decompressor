8bb762557e33e12534fe277512fd88a272af327e
Ole-Henning Undrum <oleundrum@hotmail.com>
2018-11-13 17:22:37 +0100

    Update Program.cs

--- CompressApp/Program.cs
+++ CompressApp/Program.cs
@@ -10,8 +10,7 @@ using System.Threading.Tasks;
 namespace CompressApp
 {
     class Program
-    {
-        
+    {    
         static void Main(string[] args)
         {
             Console.Write("Type in file path: ");
